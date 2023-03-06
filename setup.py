c0b4e98cb5f1573b91caeb9dc0b1ff2bbf07c0ff
Peruz <lucaperuzzo@outlook.com>
2023-03-06 16:29:30 +0100

    update init

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from distutils.core import setup
 
-setup(name='Electra Processing',
-      version='1.0',
-      description='An application to process Electra .ele files, GUI or CLI',
-      author='Luca Peruzzo',
-      url='https://github.com/Peruz/ElectraProcessing.git',
-      packages=['electra_processing'],
-      package_dir={'electra_processing': './electra_processing'}
-      package_data={'electra_processing': 'tests/*.ele'}
+setup(
+    name='Electra Processing',
+    version='1.0',
+    description='An application to process Electra .ele files, GUI or CLI',
+    author='Luca Peruzzo',
+    url='https://github.com/Peruz/ElectraProcessing.git',
+    packages=['electra_processing'],
+    package_dir={'electra_processing': './electra_processing'},
+    package_data={'electra_processing': 'tests/*.ele'}
+    install_requires=[
+        'PySide6>=6.3.1',
+        'pandas>=1.4.3',
+        'numpy',
+        'numba',
+        'matplotlib',
+        'datetime',
+        'argparse',
+
+    ]
 )
